90d2cbb8546e2217ebf0ec23016517f53d4c5c72
Conor McNally <conor_mcnally@rapid7.com>
2021-11-30 21:58:08 +0000

    Connection keys input

--- plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/connection/connection.py
+++ plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/connection/connection.py
@@ -1,3 +1,5 @@
+import base64
+
 import insightconnect_plugin_runtime
 from .schema import ConnectionSchema, Input
 # Custom imports below
@@ -7,20 +9,19 @@ class Connection(insightconnect_plugin_runtime.Connection):
 
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
+        self.test_url = "https://management.api.umbrella.com/v1"
 
     def connect(self, params):
-        """
-        Connection config params are supplied as a dict in
-        params or also accessible in self.parameters['key']
-
-        The following will setup the var to be accessed
-          self.blah = self.parameters['blah']
-        in the action and trigger files as:
-          blah = self.connection.blah
-        """
-        # TODO: Implement connection or 'pass' if no connection is necessary
         self.logger.info("Connect: Connecting...")
+        self.apiKey = params.get(Input.API_KEY).get("api_key")
+        self.apiSecretKey = params.get(Input.API_SECRET).get("api_secret")
+        self.orgId = params.get(Input.ORGANIZATION_ID).get("organization_id")
+
+        #Encode to base64 | Combine api_key & secret_key with ':' and encode
+        self.combinedKey = self.apiKey + ":" + self.apiSecretKey
+        self.encodedKey = base64.b64encode(self.combinedKey.encode("utf-8"))
+
+
 
     def test(self):
-        # TODO: Implement connection test
         pass
