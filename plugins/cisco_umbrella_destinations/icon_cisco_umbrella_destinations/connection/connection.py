f52e42a2c3c840834d686b03311d84a514aa4e01
Conor McNally <conor_mcnally@rapid7.com>
2021-11-30 22:42:18 +0000

    changed bytes string to string

--- plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/connection/connection.py
+++ plugins/cisco_umbrella_destinations/icon_cisco_umbrella_destinations/connection/connection.py
@@ -1,7 +1,11 @@
 import base64
-
+import requests
 import insightconnect_plugin_runtime
+
+
 from .schema import ConnectionSchema, Input
+
+
 # Custom imports below
 
 
@@ -9,7 +13,7 @@ class Connection(insightconnect_plugin_runtime.Connection):
 
     def __init__(self):
         super(self.__class__, self).__init__(input=ConnectionSchema())
-        self.test_url = "https://management.api.umbrella.com/v1"
+        self.test_url = f"https://management.api.umbrella.com/v1/organizations/{self.orgId}/destinationlists"
 
     def connect(self, params):
         self.logger.info("Connect: Connecting...")
@@ -17,11 +21,22 @@ class Connection(insightconnect_plugin_runtime.Connection):
         self.apiSecretKey = params.get(Input.API_SECRET).get("api_secret")
         self.orgId = params.get(Input.ORGANIZATION_ID).get("organization_id")
 
-        #Encode to base64 | Combine api_key & secret_key with ':' and encode
+        # Encode to base64 | Combine api_key & secret_key with ':' and encode
         self.combinedKey = self.apiKey + ":" + self.apiSecretKey
-        self.encodedKey = base64.b64encode(self.combinedKey.encode("utf-8"))
-
-
+        self.encodedKey = base64.b64encode(self.combinedKey.encode("utf-8")).decode('utf-8')
 
     def test(self):
-        pass
+
+        headers = {"Authorization": f"Basic {self.encodedKey}", "Content-Type": "application/json"}
+        response = requests.get(self.test_url, headers=headers)
+
+        if response.status_code == 200:
+            return True
+        elif response.status_code == 402:
+            raise Exception("Invalid API Authentication")
+        elif response.status_code == 404:
+            raise Exception("Not found")
+        elif response.status_code == 429:
+            raise Exception("Rate Limit reached")
+        else:
+            return False
